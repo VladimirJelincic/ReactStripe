722f04463af9989835d7e9581b4f27a7ad24a778
Vladimir Jelicic <vladimir.jelincic@gmail.com>
2017-10-30 21:29:28 +0100

    fixed typo

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
-      proky: true
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
